6057ad21d8f2cd84d089af8ba5116ebc72f32e07
K0rh <111435820+K0rh@users.noreply.github.com>
2022-10-01 17:57:01 +0200

    Ajout Popup pour recettes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,40 +1,48 @@
 import React, { useState } from "react";
 
 export default function Recipe(props) {
-    const [isTextVisible, setTextVisible] = useState("false");
+    const [isTextHidden, setTextVisible] = useState("true");
 
     const ingredientsList = props.ingredients.map((ingredient, index) => {
         return <li key={index}>{ingredient}</li>;
     })
 
-    const recipeClicked = () => {
-        setTextVisible(!isTextVisible);
+    const recipeOpen = () => {
+        setTextVisible(false);
+    }
+
+    const recipeClosed = () => {
+        setTextVisible(true);
     }
 
     const recipeText = (
-        <div className="recipeContainer">
-            <div className="ingredients">
-                <h3>Ingrédients</h3>
-                <ul>
-                    {ingredientsList}
-                </ul>
-            </div>
-            <div className="texteRecette">
-                <h3>Recette {props.details}</h3>
-                <p>{props.recipe}</p>
-            </div>
-            <div className="imgRecette">
-                <img src={props.image} alt="Oups non disponible" />
+        <div className="popup">
+            <div className="recipeContainer">
+                <h2 className="recipeNameOnPopup">{props.name}</h2>
+                <div className="ingredients">
+                    <h3>Ingrédients</h3>
+                    <ul>
+                        {ingredientsList}
+                    </ul>
+                </div>
+                <div className="texteRecette">
+                    <h3>Recette {props.details}</h3>
+                    <p>{props.recipe}</p>
+                </div>
+                <div className="imgRecette">
+                    <img src={props.image} alt="Oups non disponible" />
+                </div>
+                <div className="closeRecipeButton">
+                    <button onClick={() => recipeClosed()}>X</button>
+                </div>
             </div>
         </div>);
 
     return (
-        <div>
-            <div className="imageContainer">
-                <h2 className="recipeName" onClick={() => recipeClicked()}>{props.name}</h2>
-                {isTextVisible && <img src={props.image} alt="Oups non disponible" />}
-            </div>
-            {!isTextVisible && recipeText}
+        <div className="imageContainer">
+            <h2 className="recipeName" onClick={() => recipeOpen()}>{props.name}</h2>
+            {isTextHidden && <img src={props.image} alt="Oups non disponible" />}
+            {!isTextHidden && recipeText}
         </div>
     );
 }
